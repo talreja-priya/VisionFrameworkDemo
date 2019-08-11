19d6a6925bc36923615a1410f86c885cd44a6cf5
Priya Talreja <talreja.priya95@gmail.com>
2019-08-11 17:13:11 +0530

    View changes

--- ImageAnalyzer-ML/ImageAnalyzer-ML/ViewController.swift
+++ ImageAnalyzer-ML/ImageAnalyzer-ML/ViewController.swift
@@ -11,18 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var cameraButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-    @IBAction func takePicture() {
+    @IBAction func photoButtonClicked(_ sender: UIButton) {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             presentPhotoPicker(sourceType: .photoLibrary)
             return
         }
-        
         let photoSourcePicker = UIAlertController()
         let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
             self.presentPhotoPicker(sourceType: .camera)
@@ -30,19 +26,11 @@ class ViewController: UIViewController {
         let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
             self.presentPhotoPicker(sourceType: .photoLibrary)
         }
-        
         photoSourcePicker.addAction(takePhoto)
         photoSourcePicker.addAction(choosePhoto)
         photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         present(photoSourcePicker, animated: true)
-    }
-    
-    private func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = sourceType
-        present(picker, animated: true)
     }
 }
 
@@ -52,11 +40,18 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         picker.dismiss(animated: true)
         
         guard let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            fatalError("Error! Could not retrieve image from image picker.")
+            fatalError("Error!")
         }
         
         imageView.image = uiImage
         createVisionRequest(image: uiImage)
+    }
+    
+    private func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
     }
 }
 
